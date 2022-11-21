6e5238914a4b887d8bb2a9d6a88f69d42d3f0dc5
Vamsi-akkala <akkalavamsi.123@gmail.com>
2022-11-21 12:07:07 +0530

    s

--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
 ﻿using Microsoft.Azure.Cosmos;
-String endpoint = "";
-String key = "";
-CosmosClient client = new (endpoint,key);
+
+String connectionString = " " ;
+CosmosClient client = new (connectionString);
+Container container = client.GetContainer();
+
+string sql = " " ;
+QueryDefinition query = new  QueryDefinition(sql);
+FeedIterator<product> iterator = container.GetItemQueryIterator<product>(query);
+
+while (iterator.HasMoreResults)
+{
+
+ FeedResponse<product> products = await iterator.ReadNextAsync();
+ foreach ( Product product in products)
+ {
+     Console.WriteLine($"[ { product.name, 30}\t{product.price}]");
+ }
+}
---
+++ schema.cs
@@ -0,0 +1,14 @@
+/*
+public class sample
+{
+   public string id { get; set; }
+
+   public string run { get; set; }
+
+   public Decimal Sam { get; set; }
+
+
+}
+*/
+
+public record sample( string item, string ss,string price);
